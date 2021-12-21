c969e58aac227ede64f84c5fbc64a78239997994
Hussain Iftikhar <95127768+hussainiftikhar5242@users.noreply.github.com>
2021-12-21 15:52:50 +0500

    display congratulate if name=ali otherwise display sorry

---
+++ task6.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	string name;
+	cout<<"enter a name:";
+	cin>> name;
+	if( name == "ali")
+	{
+		cout<<"  congratulate"<< "  "<<name;
+	}
+	else 
+	{
+		cout<<"sorry try again"<<" "<< name;
+	}
+	
+}

a5d0b1a8632dd78bdb373e5fdb2ff3e54103397d
Hussain Iftikhar <95127768+hussainiftikhar5242@users.noreply.github.com>
2021-12-21 15:55:54 +0500

    this program check the word are same or not.

---
+++ task9.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	
+	string ch1,ch2;
+	cout<<"enter two words:";
+	cin>> ch1>>ch2;
+	if( ch1==ch2)
+	{
+		cout<<"words are same:";
+	}
+	else 
+	{
+		cout<<"words are not same:";
+	}
+}

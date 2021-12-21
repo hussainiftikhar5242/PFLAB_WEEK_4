1503274348426641cbbb856b1bcfdfa3264e4cb0
Hussain Iftikhar <95127768+hussainiftikhar5242@users.noreply.github.com>
2021-12-21 15:50:18 +0500

    ask cost from user and display he will buy the address or not using if else

---
+++ task4.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int price;
+	cout<<" enter a price:";
+	cin>> price;
+	if(price < 1500)
+	{
+		cout<<"you can buy the address:";
+	}
+	else 
+	{
+		cout<<"you can not buy the address:";
+	}
+	
+}

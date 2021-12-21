841c4362b9874e734d4a072e25e07160fd9f5a27
Hussain Iftikhar <95127768+hussainiftikhar5242@users.noreply.github.com>
2021-12-21 15:47:55 +0500

    ask you will go to study and then display output

---
+++ task2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	char ch;
+	cout<<"are you goning to study(press y or n)";
+	cin>> ch;
+	if (ch == 'y')
+	{
+		cout<< "then you will go to study:";
+	}
+}

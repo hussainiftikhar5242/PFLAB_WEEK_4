80aa7e22f70e6afe83e612afb523776b71e0b5ac
Hussain Iftikhar <95127768+hussainiftikhar5242@users.noreply.github.com>
2021-12-21 15:59:38 +0500

    check character is vowel, number, consonant

---
+++ task12.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	char ch;
+	cout<<"enter a character:";
+	cin>> ch;
+	if(ch=='a')
+	{
+		cout<<"it is vowel:";
+	}
+	else if(ch=='A')
+	{
+		cout<<"it is vowel:";
+	}
+	else if(ch=='e')
+	{
+		cout<<"it is vowel:";
+	}
+	else if(ch=='E')
+	{
+		cout<<"it is vowel:";
+	}
+	else if(ch=='i')
+	{
+		cout<<"it is vowel:";
+	}
+	else if(ch=='I')
+	{
+		cout<<"it is vowel:";
+	}
+	else if(ch=='o')
+	{
+		cout<<"it is vowel:";
+	}
+	else if(ch=='O')
+	{
+		cout<<"it is vowel:";
+	}
+	else if(ch=='u')
+	{
+		cout<<"it is vowel:";
+	}
+	else if(ch=='U')
+	{
+		cout<<"it is vowel:";
+	}
+	else 
+	{
+		cout<<"it is consonant or number:";
+	}
+
+} 
+
+
+
+
+
+
+
+
+
+
+
+

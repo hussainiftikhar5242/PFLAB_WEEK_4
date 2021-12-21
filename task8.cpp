925c54559f23b7cde4bb4dfb0ce15820584eb867
Hussain Iftikhar <95127768+hussainiftikhar5242@users.noreply.github.com>
2021-12-21 15:54:35 +0500

    display which number is greater.

---
+++ task8.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int num1,num2;
+	cout<<"enter two numbers:";
+	cin>> num1;
+	cin>> num2;
+	if(num1>num2)
+	{
+		cout<<num1<<"  "<<"is greater";
+	}
+	else 
+	{
+		cout<<num2<<" "<< "is greater than ";
+	}
+} 
+	

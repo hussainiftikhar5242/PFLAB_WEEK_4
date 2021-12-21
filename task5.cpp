951e63355c13af9e4f31a21a11791c71110f9926
Hussain Iftikhar <95127768+hussainiftikhar5242@users.noreply.github.com>
2021-12-21 15:51:37 +0500

    ask value and display go to study or not.

---
+++ task5.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	char ch;
+	cout<<"are you goning to study(press y or n)";
+	cin>> ch;
+	if (ch == 'y')
+	{
+		cout<< "then you will go to study:";
+	}
+	else 
+	{
+		cout<<"you can not go to study:";
+	}
+}
